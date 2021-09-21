0ad97efb468d2c713ab95d0368be06609dfa9efa
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-09-20 20:43:46 -0500

    Commit

--- Statistics/settings.py
+++ Statistics/settings.py
@@ -129,10 +129,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'my_static_files'),
-    )
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
